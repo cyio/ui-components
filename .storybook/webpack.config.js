525b54bbb9a40b549af6e1c80d91db82bade04b3
cyio <icaner@qq.com>
2019-03-11 18:21:15 +0800

    addon: vue with info, action

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -9,6 +9,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.vue$/,
+        loader: 'storybook-addon-vue-info/loader',
+        enforce: 'post'
+      },
+      {
         test: /.scss$/,
         exclude: /node_modules/,
         loaders: [{ loader: 'style-loader' }, { loader: 'css-loader' },
--- src/components/VInputText/index.stories.js
+++ src/components/VInputText/index.stories.js
@@ -1,51 +1,71 @@
-import { storiesOf } from '@storybook/vue'
-// import VueInfoAddon from 'storybook-addon-vue-info'
+import { storiesOf, addDecorator } from '@storybook/vue'
+import { action } from '@storybook/addon-actions';
+import { withInfo } from 'storybook-addon-vue-info'
 import VInputText from '.'
 
 const wrapper = {
   components: { VInputText },
   propsDescription: {
-    type: `Type of the text input. Options ['text', 'email', 'url', 'tel', 'search', 'password']`,
-    disabled: 'Disables the input by adding "disabled" attribute',
-    value: 'Value of the input',
-    required: 'Adds the required attribute to input',
-    placeholder: 'Placeholder text for the input',
-    name: 'name attribute for input',
-    pattern: 'pattern attribute for input (used for mobile keypad)',
-    autocomplete: 'Autocomplete attribute used for browser autocomplete',
-    inputmode: 'inputmode attribute for input (used for mobile keypad)',
-    id: 'Id attribute for the input'
+    VInputText: {
+      type: `Type of the text input. Options ['text', 'email', 'url', 'tel', 'search', 'password']`,
+      disabled: 'Disables the input by adding "disabled" attribute',
+      value: 'Value of the input',
+      required: 'Adds the required attribute to input',
+      placeholder: 'Placeholder text for the input',
+      name: 'name attribute for input',
+      pattern: 'pattern attribute for input (used for mobile keypad)',
+      autocomplete: 'Autocomplete attribute used for browser autocomplete',
+      inputmode: 'inputmode attribute for input (used for mobile keypad)',
+      id: 'Id attribute for the input'
+    }
   }
 }
 
+export const methods = {
+  onFocus: action('onFocus'),
+};
+
 storiesOf('Atom - VInputText', module)
-  // .addDecorator(VueInfoAddon)
-  .add('default', () => ({
-    ...wrapper,
-    template: `
+  .addDecorator(withInfo)
+  .add(
+    'default',
+    () => ({
+      ...wrapper,
+      template: `
       <div>
-        <VInputText type="text" defaultValue="40" v-model="inputValue" />
+        <VInputText type="text" defaultValue="40" v-model="inputValue" @focus="onFocus"/>
 
         <br/><br/>
         Value is: {{ inputValue }}
       </div>
     `,
-    data () {
-      return {
-        inputValue: 'Default Value'
-      }
+      data () {
+        return {
+          inputValue: 'Default Value'
+        }
+      },
+      methods
+    }),
+    {
+      info: true
     }
-  }))
-  .add('type', () => ({
-    ...wrapper,
-    template: `
+  )
+  .add(
+    'type',
+    () => ({
+      ...wrapper,
+      template: `
       <div>
         <VInputText type="password" v-model="inputValue" />
       </div>
     `,
-    data () {
-      return {
-        inputValue: ''
+      data () {
+        return {
+          inputValue: ''
+        }
       }
+    }),
+    {
+      info: true
     }
-  }))
+  )
